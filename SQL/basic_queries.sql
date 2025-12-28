3fec29ff2d9ecb49f3963bacae9b25d7dfc75b63
chaditi692-prog <chaditi692@gmail.com>
2025-12-28 15:12:06 +0530

    Cleaned up basic SQL queries

--- SQL/basic_queries.sql
+++ SQL/basic_queries.sql
@@ -1,7 +1,18 @@
 -- Basic SQL practice
 
+-- Selecting all columns
 SELECT * FROM customers;
 
+-- Selecting specific columns
 SELECT name, country FROM customers;
 
+-- Counting rows
 SELECT COUNT(*) FROM orders;
+
+-- Filtering data
+SELECT * FROM customers WHERE country = 'India';
+
+-- Grouping data
+SELECT country, COUNT(*)
+FROM customers
+GROUP BY country;
